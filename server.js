167621842f7cca750c341a2b8aae70eef80df273
Prajankya <prajankya@gmail.com>
2018-03-16 13:45:59 +0530

    Cleaned, moved, and now finally working code

--- server.js
+++ server.js
@@ -2,60 +2,73 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 
 const SECRET = "SECRET...."
 
+app.use(cookieParser())
 app.use(express.json()) // to support JSON-encoded bodies
 app.use(express.urlencoded()) // to support URL-encoded bodies
 
-var auth = function (req, res, next) {
-  var token = req.headers['x-access-token'];
-  if (!token) {
-
-    return res.status(401).send({
-      auth: false,
-      message: 'No token provided.'
-    });
-  }
-
-  jwt.verify(token, SECRET, function (err, decoded) {
-    if (err) return res.status(500).send({
-      auth: false,
-      message: 'Failed to authenticate token.'
-    });
-  })
-  next()
-}
-
 app.post('/auth', function (req, res, next) {
-
   if (req.body.username == 'admin' && req.body.password == 'admin') {
     // create a token
     var token = jwt.sign({
       id: req.body.username
     }, SECRET, {
       expiresIn: 86400 // expires in 24 hours
-    });
-    res.status(200).send({
-      auth: true,
-      token: token
-    });
+    })
+
+    res.cookie('id_token', token, {
+      expires: new Date(Date.now() + 36000),
+      httpOnly: true
+    })
+
+    //For proper applications also add the same into database
+
+    res.redirect('/admin') //logged in and token saved as cookie
   } else {
     res.sendFile(path.join(__dirname, 'views', 'fail.html'))
   }
 })
-//
-// app.get('/private', function (req, res, next) {
-//   res.sendFile(path.join(__dirname, 'views', 'index.html'))
-// })
-//
-// app.get('/logout', function (req, res, next) {
-//   res.sendFile(path.join(__dirname, 'views', 'index.html'))
-// })
 
-// app.use('/admin', jwt({
-//   secret: 'secret'
-// })) //check for jwt for all pages under /admin
+//check login for all pages under /admin
+app.use('/admin', function (req, res, next) {
+  var token = undefined;
+
+  if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+    token = req.headers.authorization.split(' ')[1]
+  } else if (req.headers['x-access-token']) {
+    token = req.headers['x-access-token']
+  } else if (req.cookies && req.cookies.id_token) {
+    token = req.cookies.id_token
+  }
+
+  if (!token) {
+    //not logged in so send login page
+    // res.redirect('/login.html')
+    // return
+    return res.status(401).send('Not logged in')
+  } else {
+    jwt.verify(token, SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(500).send({
+          auth: false,
+          message: 'Failed to authenticate token.'
+        })
+      } else {
+        next() //Authenticated ! 👍
+      }
+    })
+  }
+})
+
+app.get('/admin/logout', function (req, res, next) {
+  //For now just deleting the cookie,
+  //but for proper applications, delete the token from database as well
+  res.cookie('id_token', "")
+  res.end("<h1>you are logged out</h1><a href='/'>Go back to homepage</a>")
+})
 
 app.use(express.static('views')) //Send all the static files
 
